be606b03a3309ae8215bd0f1c0d949b7aa7cc5e8
Arthur Gomes de Souza <126431991+Arthur-Gomes-de-Souza@users.noreply.github.com>
2023-10-21 19:02:55 -0300

    Temperature converter created

--- javascript/pratica/script.js
+++ javascript/pratica/script.js
@@ -1,20 +1,23 @@
-function tempConvert() {
-    var temperatura = parseFloat(document.getElementById("temperatura").value);
-    var resposta = document.getElementById("resposta");
+const celsius = document.querySelector("#celsius");
+const fahrenheit = document.querySelector("#fahrenheit");
 
-    if (document.getElementById('celsius').checked == true) {
-        resposta.innerTMHL = cToF(temperatura);
+// COLOCANDO O FOCO NO CELSIUS QUANDO ATUALIZAR A PÁGINA:
+window.addEventListener("load", () => celsius.focus());
+
+// CONVERTER CELSIUS PARA FAHRENHEIT QUANDO O VALOR DE CELSIUS MUDAR:
+celsius.addEventListener("input", () => {
+    fahrenheit.value = ((celsius.value * 9) / 5 + 32).toFixed(2);
+    // LIMPAR O FAHRENHEIT QUANDO O CELSIUS ESTIVER LIMPO:
+    if (!celsius.value) {
+        fahrenheit.value = "";
     }
+});
 
-    if (document.getElementById('fahrenheit').checked == true) {
-        resposta.innerHTML = fToC(temperatura);
+// CONVERTER FAHRENHEIT PARA CELSIUS QUANDO O VALOR DE CELSIUS MUDAR:
+fahrenheit.addEventListener("input", () => {
+    celsius.value = (((fahrenheit.value - 32) * 5) / 9).toFixed(2);
+    // LIMPAR O CELSIUS QUANDO O FAHRENHEIT ESTIVER LIMPO:
+    if (!fahrenheit.value) {
+        celsius.value = "";
     }
-}
-
-function cToF (x) {
-    return (x * 9/5) + 32;
-}
-
-function fToC (x) {
-    return (x - 32) * 5/9;
-}
+});
